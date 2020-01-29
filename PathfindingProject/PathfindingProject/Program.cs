1c6ccd1968cc64d52e69343f64b0fc6d938cad74
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2020-01-29 11:43:33 -0600

    Initial in Class commit
    In class commit with some Psudo code

---
+++ PathfindingProject/PathfindingProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PathfindingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // number of moves to easily compare which path is more efficient
+            int numOfMoves = 0;
+
+
+            // Create 2D array for the grid
+        }
+    }
+}
